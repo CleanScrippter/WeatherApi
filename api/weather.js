aec0bda44e95275bf64f760c01b5c21d91d0a72b
Deepak Rajput <createcode2gether@gmail.com>
2026-02-11 00:04:27 +0530

    cors enabled

--- api/weather.js
+++ api/weather.js
@@ -1,6 +1,21 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
+    // CORS Headers
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+
+    // Handle OPTIONS request
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     const { city } = req.query;
 
     if (!city) {
